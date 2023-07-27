7903650bb06b105b184542e8dfa488850186bef6
Bryan <bryan.andion@gmail.com>
2023-07-26 20:30:56 -0500

    update

--- Cerberus_v.0.1/Presentacion/Program.cs
+++ Cerberus_v.0.1/Presentacion/Program.cs
@@ -14,7 +14,7 @@ namespace Presentacion
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Frm_login());
+            Application.Run(new Frm_inicio());
         }
     }
 }
